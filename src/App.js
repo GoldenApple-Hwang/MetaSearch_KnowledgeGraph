9959265a5d12af56ec5ad63ef5de39cacadd2f7c
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-06 15:12:01 +0900

    feat: neo4j에서 가져온 노드 정보와 관계정보를 바탕으로 화면에 그래프 그리기

--- src/App.js
+++ src/App.js
@@ -4,36 +4,95 @@ import './App.css';
 import { useParams } from 'react-router-dom';
 
 function App() {
-  
+
   const svgRef = useRef();
-  
+
   const width = 1440; //1440 × 2560
   const height = 1440;
-  
+
   const { dbName } = useParams();
   console.log(dbName);
-  
+
   const [graphData, setGraphData] = useState({ nodes: [], links: [] }); // neo4j에서 그래프를 그릴 데이터를 받아와서 저장
 
   useEffect(() => {
     // dbName 값이 설정된 경우에만 데이터를 가져옴
-  if (dbName) {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://223.194.154.167/api/graphData/${dbName}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+    if (dbName) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`http://223.194.154.167/api/graphData/${dbName}`);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          setGraphData(data);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
         }
-        const data = await response.json();
-        setGraphData(data);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-  
-    fetchData();
-  }
+      };
+
+      fetchData();
+    }
   }, [dbName]);
+
+  // 이 코드는 그래프를 그리는 코드
+  useEffect(() => {
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const links = graphData.links.map((d) => ({ ...d }));
+    const nodes = graphData.nodes.map((d) => ({ ...d }));
+    console.log(links);
+    console.log(nodes);
+
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3.forceLink(links).id((d) => d.id) //.distance(100)
+      )
+      .force("charge", d3.forceManyBody().strength(-300))
+      .force("collide", d3.forceCollide(10)) //.strength(0.7))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+
+    const svg = d3.select(svgRef.current);
+
+    svg
+      .attr("viewBox", [-width / 2, -height / 2, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
+
+    const link = svg
+      .append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(links)
+      .join("line")
+      .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+    const node = svg
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+      .attr("r", 15)
+      .attr("fill", (d) => color(d.group))
+
+      simulation.on("tick", () => {
+        link
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+  
+        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+      });
+
+    // Cleanup on component unmount
+    return () => simulation.stop();
+  }, [graphData]);
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/graph/:dbName' element={<App/>}></Route>
+      {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+			{/*<Route path="*" element={<NotFound />}></Route>*/}
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
