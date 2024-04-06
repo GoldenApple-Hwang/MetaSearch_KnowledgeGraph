03c8f50a1ad6a765a9cac3e43fb644e419495e2b
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-06 14:50:20 +0900

    feat: DB 이름을 서버에 보내고 해당 DB의 node와 link에 관한 데이터를 json 형태로 받아옴

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
 import './App.css';
+import { useParams } from 'react-router-dom';
 
 function App() {
+  
   const svgRef = useRef();
-
+  
   const width = 1440; //1440 × 2560
   const height = 1440;
+  
+  const { dbName } = useParams();
+  console.log(dbName);
+  
+  const [graphData, setGraphData] = useState({ nodes: [], links: [] }); // neo4j에서 그래프를 그릴 데이터를 받아와서 저장
+
+  useEffect(() => {
+    // dbName 값이 설정된 경우에만 데이터를 가져옴
+  if (dbName) {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://223.194.154.167/api/graphData/${dbName}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setGraphData(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+  
+    fetchData();
+  }
+  }, [dbName]);
 
   return (
     <div className="App">
