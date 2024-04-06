411771cf141c17e46a04bb0ae4935f77bd11f20c
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-06 14:43:56 +0900

    feat: 그래프를 그리게 될 svg 엘리먼트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useRef } from "react";
 import './App.css';
 
 function App() {
+  const svgRef = useRef();
+
+  const width = 1440; //1440 × 2560
+  const height = 1440;
+
   return (
     <div className="App">
+      <svg ref={svgRef} width={width} height={height} />
     </div>
   );
 }
