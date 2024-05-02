14de3323300629a0a817103bd1cbf05fa0579295
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-02 14:08:52 +0900

    feat: PeopleBubbleChartPage를 라우트에 추가
    - /personbubble/:dbName 경로에서 PeopleBubbleChartPage를 보여주기 위해 라우터 설정에 추가
    - PeopleBubbleChartPage 컴포넌트는 지정된 dbName을 사용해 메타데이터 지식그래프에서의 인물 빈도를 시각화

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import PeopleBubbleChartPage from './pages/PeopleBubbleChartPage.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
       <Route path='/graph/:dbName' element={<App/>}></Route>
       {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 			{/*<Route path="*" element={<NotFound />}></Route>*/}
+      <Route path='/personbubble/:dbName' element={<PeopleBubbleChartPage/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
