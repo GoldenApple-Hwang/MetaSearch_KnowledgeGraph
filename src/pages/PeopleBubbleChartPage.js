1370ea18d6d9c226dd5d617c33db368ee88c6dd9
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-05 19:12:59 +0900

    refactor: 데이터를 불러오는 서버 주소의 엔드포인트 변경
    - 'http://113.198.85.4/peoplebubblechart/' 에서 'http://113.198.85.4/api/peoplebubblechart/' 로 변경

--- src/pages/PeopleBubbleChartPage.js
+++ src/pages/PeopleBubbleChartPage.js
@@ -7,7 +7,7 @@ const PeopleBubbleChartPage = () => {
     const { dbName } = useParams();  // URL에서 dbName 파라미터 추출
 
     useEffect(() => {
-        const apiUrl = `http://113.198.85.4/peoplebubblechart/${dbName}`; // 실제 배포 시 도메인 주소로 변경 필요
+        const apiUrl = `http://113.198.85.4/api/peoplebubblechart/${dbName}`; // 실제 배포 시 도메인 주소로 변경 필요
 
         const fetchData = async () => {
             try {
@@ -26,9 +26,22 @@ const PeopleBubbleChartPage = () => {
         fetchData();
     }, [dbName]); // dbName이 변경될 때마다 fetchData 함수를 다시 호출
 
+    // const data = [
+    //     { entity: "Entity10 이름", frequency: 100 },
+    //     { entity: "Entity9 이름", frequency: 90  },
+    //     { entity: "Entity8 이름", frequency: 80  },
+    //     { entity: "Entity7 이름", frequency: 70 },
+    //     { entity: "Entity6 이름", frequency: 6  },
+    //     { entity: "Entity5 이름", frequency: 5  },
+    //     { entity: "Entity4 이름", frequency: 4 },
+    //     { entity: "Entity3 이름", frequency: 3  },
+    //     { entity: "Entity2 이름", frequency: 2  },
+    //     { entity: "Entity1 이름", frequency: 1 },
+    //   ];
+
     return (
         <div>
-            <h1>People Bubble Chart for {dbName}</h1>
+            {/* <h1>People Bubble Chart for {dbName}</h1> */}
             <PeopleBubbleChart data={data} />
         </div>
     );
