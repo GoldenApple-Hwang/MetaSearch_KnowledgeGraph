bb53f53a6b5c4eb018047742a17bf780e405e15f
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-11 19:55:44 +0900

    feat : EntityTripleGraphPage 라우트 추가
    - EntityTripleGraphPage 컴포넌트를 애플리케이션 라우트에 추가하여, 특정 데이터베이스 이름과 엔티티에 대한 그래프를 동적으로 표시
    - '/entityTripleGraph/:dbName/:entity' 경로를 통해 엔티티 트리플 그래프 페이지에 접근

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import PeopleBubbleChartPage from './pages/PeopleBubbleChartPage.js'
+import EntityTripleGraphPage from './pages/EntityTripleGraphPage.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Test from './Test.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Test/>}/>
       <Route path='/graph/:dbName' element={<App/>}></Route>
       {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 			{/*<Route path="*" element={<NotFound />}></Route>*/}
       <Route path='/personbubble/:dbName' element={<PeopleBubbleChartPage/>}/>
+      <Route path='/entityTripleGraph/:dbName/:entity' element={<EntityTripleGraphPage/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
