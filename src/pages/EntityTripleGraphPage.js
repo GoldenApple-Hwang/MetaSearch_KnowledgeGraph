5ed020331e1af62d9e6589888907e97fe1ba4871
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-11 19:51:25 +0900

    feat : EntityTripleGraphPage 컴포넌트 추가
    - URL 파라미터에 따라 엔티티 트리플 그래프 데이터를 동적으로 가져와서 표시
    - useEffect와 useState를 사용하여 응답 데이터를 관리하고 상태를 업데이트
    - POST 요청을 통해 서버로부터 데이터를 가져오고, 응답이 성공적이면 상태를 업데이트
    - EntityTripleGraph 컴포넌트를 통해 가져온 데이터를 기반으로 그래프를 렌더링

---
+++ src/pages/EntityTripleGraphPage.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';  // URL 파라미터를 가져오기 위해 사용
+import EntityTripleGraph from '../components/EntityTripleGraph.js';
+
+const EntityTripleGraphPage = () => {
+    const [data, setData] = useState([]);
+    const { dbName, entity } = useParams();  // URL에서 dbName 파라미터 추출
+
+    useEffect(() => {
+        const apiUrl = 'http://113.198.85.4/graph/entitytripledata';
+        const fetchData = async () => {
+            try {
+                const response = await fetch(apiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        dbName: dbName,
+                        entityName: entity
+                    })
+                });
+                if (response.ok) {
+                    const fetchedData = await response.json();
+                    setData(fetchedData);
+                } else {
+                    throw new Error('Network response was not ok.');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [dbName, entity]); // dbName과 entity가 변경될 때마다 fetchData 함수를 다시 호출
+
+    return (
+        <div>
+            <h1>Entity Triple Graph for - {entity}</h1>
+            <EntityTripleGraph data={data} dbName={dbName}/>
+        </div>
+    );
+};
+
+export default EntityTripleGraphPage;
